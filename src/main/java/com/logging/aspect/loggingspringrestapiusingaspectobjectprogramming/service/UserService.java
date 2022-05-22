8744825b7a900549f5217eb6e9568245394e220f
WARBABANK\Abdulrahman.karaien <abdulrahman.karaien@warbabank.com>
2022-05-22 12:14:09 +0300

    fix bugs

--- src/main/java/com/logging/aspect/loggingspringrestapiusingaspectobjectprogramming/service/UserService.java
+++ src/main/java/com/logging/aspect/loggingspringrestapiusingaspectobjectprogramming/service/UserService.java
@@ -1,20 +1,13 @@
 package com.logging.aspect.loggingspringrestapiusingaspectobjectprogramming.service;
 
-import com.logging.aspect.loggingspringrestapiusingaspectobjectprogramming.controller.UserController;
 import com.logging.aspect.loggingspringrestapiusingaspectobjectprogramming.model.UserDto;
-import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    private UserController userController;
-
-    public UserService(UserController userController){
-        this.userController=userController;
-    }
 
     public UserDto login(UserDto userDto) throws Exception {
-        if(userDto.getUserName().equals("abualrhman")){
+        if(userDto.getUserName().equals(userDto.getPassword())){
             throw new Exception();
         }
         return userDto;
