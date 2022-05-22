981c9193a134556a6157c1b3e4e3ac4ee1f94602
WARBABANK\Abdulrahman.karaien <abdulrahman.karaien@warbabank.com>
2022-05-22 11:30:07 +0300

    Init spring project

---
+++ src/main/java/com/logging/aspect/loggingspringrestapiusingaspectobjectprogramming/LoggingSpringRestApiUsingAspectObjectProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.logging.aspect.loggingspringrestapiusingaspectobjectprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingSpringRestApiUsingAspectObjectProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingSpringRestApiUsingAspectObjectProgrammingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/logging/aspect/loggingspringrestapiusingaspectobjectprogramming/LoggingSpringRestApiUsingAspectObjectProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logging.aspect.loggingspringrestapiusingaspectobjectprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingSpringRestApiUsingAspectObjectProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
